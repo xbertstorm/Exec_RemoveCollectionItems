4e9fee1c5ca6579a7faa185c7711bc05481dfb43
xbertstorm5566 <xbertstorm5566@gmail.com>
2022-10-23 20:16:44 +0800

    加入專案檔案。

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_RemoveCollectionItems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> test = new List<int>();
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+            test.Add(5);
+            test.Add(10);
+            test.Add(15);
+            test.Add(20);
+            test.Add(1);
+            test.Add(2);
+
+            foreach (var i in test)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            test.Remove(1);
+            test.Remove(2);
+            test.Remove(3);
+            test.RemoveAt(6); 
+
+            foreach (var i in test)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
